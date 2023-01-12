fb7254f0069ba9e504572d72b38ce82a8d62d255
Bayram Akay <akay.bayram.1461@gmail.com>
2023-01-12 20:50:34 +0300

    eklenti bugu duzeltildi

--- love_eklenti.lua
+++ love_eklenti.lua
@@ -2,21 +2,28 @@ local basilan_tuslar = {}
 local birakilan_tuslar = {}
 
 function love.keypressed(tus)
-    basilan_tuslar[tus] = true
+    basilan_tuslar[tus] = love.timer.getTime() * 1000
 end
 
 function love.keyreleased(tus)
-    birakilan_tuslar[tus] = true
+    birakilan_tuslar[tus] = love.timer.getTime() * 1000
+end
+
+local function tablo_degerini_kontrol_et(tablo, tus)
+    local zaman = love.timer.getTime() * 1000
+
+    if zaman - tablo[tus] < 200 then
+        return true
+    end
+
+    return false
 end
 
 function love.keyboard.isPressed(tus)
-    local deger = basilan_tuslar[tus]
-    basilan_tuslar[tus] = false
-    return deger
+    return tablo_degerini_kontrol_et(basilan_tuslar, tus)
 end
 
 function love.keyboard.isReleased(tus)
-    local deger = birakilan_tuslar[tus]
-    birakilan_tuslar[tus] = false
-    return deger
+    return tablo_degerini_kontrol_et(birakilan_tuslar, tus)
 end
+
