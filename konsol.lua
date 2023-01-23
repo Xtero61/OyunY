3313ef782c8cc3f23cff8d100fb10a07a0112eec
Xtero61 <oakay626@gmail.com>
2023-01-24 00:14:20 +0300

    konsol metni yapıldı

--- konsol.lua
+++ konsol.lua
@@ -6,6 +6,10 @@ local Konsol = {}
 Konsol.__index = Konsol
 Konsol.__newindex = YENI_INDEKS_UYARISI
 
+Konsol.metin = {}
+Konsol.metin.durum = false
+Konsol.metin.yazi = ""
+Konsol.metin.boyu = 14
 Konsol.yaziSon = ""
 Konsol.yazi = ""
 Konsol.gonder_yazi = ""
@@ -20,7 +24,7 @@ local gecikme = 0.7
 love.keyboard.setKeyRepeat(true)
 
 function Konsol:guncelle(dt)
-    if love.keyboard.isPressed("escape") then
+    if love.keyboard.isPressed("escape") then -- konsol açıp kapama
         if Konsol.durum == true then
             Konsol.durum = false
         else
@@ -28,30 +32,38 @@ function Konsol:guncelle(dt)
             Konsol.ayrac:ayrac_zamanlayici_sifirla()
         end
     end
-
     if Konsol.durum then
-        if love.keyboard.isPressed("return") then
+        Konsol.metin.durum = true
+        if love.keyboard.isPressed("return") then -- yazılan yazıyı gönderme 
             Konsol.gonder_yazi = Konsol.yazi .. Konsol.yaziSon
+            Konsol.metin.yazi = Konsol.metin.yazi .. "| " .. Konsol.gonder_yazi .. "\n"
+            if Konsol.metin.boyu <= -126 then -- metinin ilk satırını silme
+                Konsol.metin.yazi = string.gmatch(Konsol.metin.yazi, "[^\n]+\n(.+)")()
+            else
+            Konsol.metin.boyu = Konsol.metin.boyu - 14
+            end
             Konsol.yazi = ""
             Konsol.yaziSon = ""
         end
-        if love.keyboard.isPressed("backspace") then
+        if love.keyboard.isDown("lctrl") and love.keyboard.isPressed("t") then -- yazışma kutucuğunu temizleme
+            Konsol.metin.yazi = ""
+            Konsol.metin.boyu = 14
+        end
+        if love.keyboard.isPressed("backspace") then -- yazılan yazının sonundaki harfi silme
             Konsol.ayrac:ayrac_zamanlayici_sifirla()
             local bit_uzunlugu = utf8.offset(Konsol.yazi, -1)
             if bit_uzunlugu then
-                -- remove the last UTF-8 character.
-                -- string.sub operates on bytes rather than UTF-8 characters, so we couldn't do string.sub(text, 1, -2).
                 Konsol.yazi = string.sub(Konsol.yazi, 1, bit_uzunlugu - 1)
             end
         end
-        if love.keyboard.isPressed("left") then
+        if love.keyboard.isPressed("left") then -- yazılan yazıda imleci sağa kaydırma 
             Konsol.ayrac:ayrac_zamanlayici_sifirla()
             local harf_yeri = utf8.offset(Konsol.yazi, -1)
             if harf_yeri then
                 Konsol.yaziSon = string.sub(Konsol.yazi, harf_yeri, - 1) .. Konsol.yaziSon
                 Konsol.yazi = string.sub(Konsol.yazi, 1, harf_yeri - 1)
             end
-        elseif love.keyboard.isPressed("right") then
+        elseif love.keyboard.isPressed("right") then -- yazılan yazıda imleci sola kaydırma 
             Konsol.ayrac:ayrac_zamanlayici_sifirla()
             local son_yazi_ilk_harf_uzunlugu = utf8.offset(Konsol.yaziSon,2)
             if son_yazi_ilk_harf_uzunlugu then
@@ -61,7 +73,7 @@ function Konsol:guncelle(dt)
             end
         end
         local suanki_zaman = love.timer.getTime()
-        if suanki_zaman - baslama_zamani > gecikme then
+        if suanki_zaman - baslama_zamani > gecikme then -- imlecin yanıp söndüren zamanlayıcı
             baslama_zamani = suanki_zaman
             if Konsol.ayrac.gorunme == 0 then
                 Konsol.ayrac.gorunme = 1
@@ -71,16 +83,18 @@ function Konsol:guncelle(dt)
         end
     end
 end
-
 function Konsol:ciz()
     if Konsol.durum then
         Konsol:arkaplan()
         Konsol.ayrac:ayrac_cizgi()
         love.graphics.setColor(1,1,1)
-        love.graphics.print(Konsol.yazi,20,love.graphics.getHeight()-23)
+        love.graphics.print(Konsol.yazi,20,love.graphics.getHeight()-22.5)
         local genislik = love.graphics.getFont():getWidth(Konsol.yazi) + 20
-        love.graphics.print(Konsol.yaziSon,genislik,love.graphics.getHeight()-23)
-        love.graphics.print(Konsol.gonder_yazi,0,20)
+        love.graphics.print(Konsol.yaziSon,genislik,love.graphics.getHeight()-22.5)
+    end
+    if  Konsol.metin.durum == true then
+        local metin = love.graphics.newText(love.graphics.getFont(),Konsol.metin.yazi)
+        love.graphics.draw(metin,10,love.graphics.getHeight()-51.5+Konsol.metin.boyu)            
     end
 end
 
@@ -92,16 +106,20 @@ end
 function Konsol.ayrac:ayrac_cizgi()
     local genislik = love.graphics.getFont():getWidth(Konsol.yazi) + 20
     love.graphics.setColor(1,1,1,Konsol.ayrac.gorunme)
-    love.graphics.rectangle("fill",genislik,love.graphics.getHeight()-23,8,16)
+    love.graphics.rectangle("fill",genislik,love.graphics.getHeight()-22.5,8,16)
 end
 
 function Konsol:arkaplan()
     love.graphics.setColor(0,0,0,0.8)
     love.graphics.rectangle("fill",0,love.graphics.getHeight()-30,love.graphics.getWidth(),30)
+    love.graphics.setColor(0,0,0,0.5)
+    love.graphics.rectangle("fill",0,love.graphics.getHeight()-180,love.graphics.getWidth(),150)
+    love.graphics.setColor(0.5,0.5,0.5,1)
+    love.graphics.rectangle("line",0,love.graphics.getHeight()-180,love.graphics.getWidth(),150)
     love.graphics.setColor(0.5,0.5,0.5,1)
     love.graphics.rectangle("line",0,love.graphics.getHeight()-30,love.graphics.getWidth(),30)
     love.graphics.setColor(0,0,1,1)
-    love.graphics.polygon("fill", 10, 10+love.graphics.getHeight()-30, 15, 15+love.graphics.getHeight()-30, 10, 20+love.graphics.getHeight()-30)
+    love.graphics.polygon("fill", 10, 10+love.graphics.getHeight()-30, 16, 15+love.graphics.getHeight()-30, 10, 20+love.graphics.getHeight()-30)
 end
 
 function love.textinput(t)
