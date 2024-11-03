fba9a263bb3d300ec817ec6242499ee96010e887
akaybayram <akaybayram1461@gmail.com>
2024-11-03 19:41:16 +0300

    oyuncu vurus kutusu ve isim yerlestirmesi duzeltildi

--- kutuphane/konsol.lua
+++ kutuphane/konsol.lua
@@ -10,11 +10,11 @@ Konsol.__newindex = YENI_INDEKS_UYARISI
 Konsol.yazi_gonderme_fonksiyonlari = {}
 Konsol.metin = {}
 Konsol.metin.durum = false
-Konsol.metin.zamanlayici = zamanlayici.yeni{
+Konsol.metin.zamanlayici = zamanlayici.yeni {
     sure = 5000,
     tekrar = false,
-    arg = {Konsol.metin},
-    tetik_fonksiyonu = function (metin)
+    arg = { Konsol.metin },
+    tetik_fonksiyonu = function(metin)
         if not Konsol.durum then
             metin.durum = false
         end
@@ -30,12 +30,12 @@ Konsol.durum = false
 Konsol.ayrac = {}
 Konsol.ayrac.transparanlik = 1
 
-Konsol.ayrac.zamanlayici = zamanlayici.yeni{
+Konsol.ayrac.zamanlayici = zamanlayici.yeni {
     sure = 750,
     tekrar = true,
-    arg = {Konsol.ayrac},
-    tetik_fonksiyonu = function (ayrac)
-        if ayrac.transparanlik ==  1 then
+    arg = { Konsol.ayrac },
+    tetik_fonksiyonu = function(ayrac)
+        if ayrac.transparanlik == 1 then
             ayrac.transparanlik = 0
         else
             ayrac.transparanlik = 1
@@ -56,15 +56,15 @@ function Konsol:guncelle(dt)
         if Konsol.durum == true then
             Konsol.durum = false
             Konsol.metin.zamanlayici:yeniden_kur()
-	        sinyal_ver("Konsol.durum_degisti", Konsol.durum)
+            sinyal_ver("Konsol.durum_degisti", Konsol.durum)
         else
             Konsol.durum = true
             Konsol.metin.durum = true
-	        sinyal_ver("Konsol.durum_degisti", Konsol.durum)
+            sinyal_ver("Konsol.durum_degisti", Konsol.durum)
         end
     end
     if Konsol.durum then
-        if love.keyboard.isPressed("return") then -- yazılan yazıyı gönderme 
+        if love.keyboard.isPressed("return") then -- yazılan yazıyı gönderme
             Konsol.gonder_yazi = Konsol.yazi .. Konsol.yaziSon
             Konsol.yazi = ""
             Konsol.yaziSon = ""
@@ -77,22 +77,28 @@ function Konsol:guncelle(dt)
         if love.keyboard.isDown("lctrl") and love.keyboard.isPressed("v") then -- yapıştır
             Konsol.yazi = Konsol.yazi .. love.system.getClipboardText()
         end
+        if love.keyboard.isPressed("f3") then -- yapıştır
+            Konsol.yazi = Konsol.yazi .. "/oyun_kur 30000 10"
+        end
+        if love.keyboard.isPressed("f4") then -- yapıştır
+            Konsol.yazi = Konsol.yazi .. "/baglan 127.0.0.1:30000"
+        end
         if love.keyboard.isPressed("backspace") then -- yazılan yazının sonundaki harfi silme
             local bit_uzunlugu = utf8.offset(Konsol.yazi, -1)
             if bit_uzunlugu then
                 Konsol.yazi = string.sub(Konsol.yazi, 1, bit_uzunlugu - 1)
             end
         end
-        if love.keyboard.isPressed("left") then -- yazılan yazıda imleci sağa kaydırma 
+        if love.keyboard.isPressed("left") then -- yazılan yazıda imleci sağa kaydırma
             local harf_yeri = utf8.offset(Konsol.yazi, -1)
             if harf_yeri then
-                Konsol.yaziSon = string.sub(Konsol.yazi, harf_yeri, - 1) .. Konsol.yaziSon
+                Konsol.yaziSon = string.sub(Konsol.yazi, harf_yeri, -1) .. Konsol.yaziSon
                 Konsol.yazi = string.sub(Konsol.yazi, 1, harf_yeri - 1)
             end
-        elseif love.keyboard.isPressed("right") then -- yazılan yazıda imleci sola kaydırma 
-            local son_yazi_ilk_harf_uzunlugu = utf8.offset(Konsol.yaziSon,2)
+        elseif love.keyboard.isPressed("right") then -- yazılan yazıda imleci sola kaydırma
+            local son_yazi_ilk_harf_uzunlugu = utf8.offset(Konsol.yaziSon, 2)
             if son_yazi_ilk_harf_uzunlugu then
-	        son_yazi_ilk_harf_uzunlugu = son_yazi_ilk_harf_uzunlugu - 1
+                son_yazi_ilk_harf_uzunlugu = son_yazi_ilk_harf_uzunlugu - 1
                 Konsol.yazi = Konsol.yazi .. string.sub(Konsol.yaziSon, 1, son_yazi_ilk_harf_uzunlugu)
                 Konsol.yaziSon = string.sub(Konsol.yaziSon, son_yazi_ilk_harf_uzunlugu + 1, -1)
             end
@@ -104,19 +110,19 @@ function Konsol:ciz()
     if Konsol.durum then
         Konsol:arkaplan()
         Konsol.ayrac:ayrac_cizgi()
-        love.graphics.setColor(1,1,1)
-        love.graphics.print(Konsol.yazi,20,love.graphics.getHeight()-22.5)
+        love.graphics.setColor(1, 1, 1)
+        love.graphics.print(Konsol.yazi, 20, love.graphics.getHeight() - 22.5)
         local genislik = love.graphics.getFont():getWidth(Konsol.yazi) + 20
-        love.graphics.print(Konsol.yaziSon,genislik,love.graphics.getHeight()-22.5)
+        love.graphics.print(Konsol.yaziSon, genislik, love.graphics.getHeight() - 22.5)
     end
     if Konsol.metin.durum == true then
-        local metin = love.graphics.newText(love.graphics.getFont(),Konsol.metin.yazi)
-        love.graphics.draw(metin,7,love.graphics.getHeight()-51.5+Konsol.metin.boyu)
+        local metin = love.graphics.newText(love.graphics.getFont(), Konsol.metin.yazi)
+        love.graphics.draw(metin, 7, love.graphics.getHeight() - 51.5 + Konsol.metin.boyu)
     end
 end
 
-function Konsol.metin:metine_yazi_ekle(isim,yazi)
-    Konsol.metin.yazi = Konsol.metin.yazi .. isim .." : " .. yazi .. "\n"
+function Konsol.metin:metine_yazi_ekle(isim, yazi)
+    Konsol.metin.yazi = Konsol.metin.yazi .. isim .. " : " .. yazi .. "\n"
     Konsol.metin:metinin_boyunu_ayarlama()
     Konsol.metin.durum = true
 end
@@ -137,21 +143,22 @@ end
 
 function Konsol.ayrac:ayrac_cizgi()
     local genislik = love.graphics.getFont():getWidth(Konsol.yazi) + 20
-    love.graphics.setColor(1,1,1,Konsol.ayrac.transparanlik)
-    love.graphics.rectangle("fill",genislik,love.graphics.getHeight()-22.5,8,16)
+    love.graphics.setColor(1, 1, 1, Konsol.ayrac.transparanlik)
+    love.graphics.rectangle("fill", genislik, love.graphics.getHeight() - 22.5, 8, 16)
 end
 
 function Konsol:arkaplan()
-    love.graphics.setColor(0,0,0,0.8)
-    love.graphics.rectangle("fill",0,love.graphics.getHeight()-30,love.graphics.getWidth(),30)
-    love.graphics.setColor(0,0,0,0.5)
-    love.graphics.rectangle("fill",0,love.graphics.getHeight()-180,love.graphics.getWidth(),150)
-    love.graphics.setColor(0.5,0.5,0.5,1)
-    love.graphics.rectangle("line",0,love.graphics.getHeight()-180,love.graphics.getWidth(),150)
-    love.graphics.setColor(0.5,0.5,0.5,1)
-    love.graphics.rectangle("line",0,love.graphics.getHeight()-30,love.graphics.getWidth(),30)
-    love.graphics.setColor(0,0,1,1)
-    love.graphics.polygon("fill", 10, 10+love.graphics.getHeight()-30, 16, 15+love.graphics.getHeight()-30, 10, 20+love.graphics.getHeight()-30)
+    love.graphics.setColor(0, 0, 0, 0.8)
+    love.graphics.rectangle("fill", 0, love.graphics.getHeight() - 30, love.graphics.getWidth(), 30)
+    love.graphics.setColor(0, 0, 0, 0.5)
+    love.graphics.rectangle("fill", 0, love.graphics.getHeight() - 180, love.graphics.getWidth(), 150)
+    love.graphics.setColor(0.5, 0.5, 0.5, 1)
+    love.graphics.rectangle("line", 0, love.graphics.getHeight() - 180, love.graphics.getWidth(), 150)
+    love.graphics.setColor(0.5, 0.5, 0.5, 1)
+    love.graphics.rectangle("line", 0, love.graphics.getHeight() - 30, love.graphics.getWidth(), 30)
+    love.graphics.setColor(0, 0, 1, 1)
+    love.graphics.polygon("fill", 10, 10 + love.graphics.getHeight() - 30, 16, 15 + love.graphics.getHeight() - 30, 10,
+        20 + love.graphics.getHeight() - 30)
 end
 
 function love.textinput(t)
--- kutuphane/kullanici_arayuz.lua
+++ kutuphane/kullanici_arayuz.lua
@@ -10,12 +10,12 @@ Kullanici_Arayuz.__newindex = YENI_INDEKS_UYARISI
 
 function Kullanici_Arayuz:yeni(o)
     o = o or {}
-    o.yer = o.yer or vektor2(0,0)
+    o.yer = o.yer or vektor2(0, 0)
     assert(o.oyuncu, "Arayuz bir oyuncuya ait olmali!!!")
     o.oyuncu = o.oyuncu
     o.elleri_goster = false
     o.el_gosterge_yer = vektor2(o.oyuncu.yer.x, o.oyuncu.yer.y)
-    o.gui_zamanlayici = zamanlayici.yeni{
+    o.gui_zamanlayici = zamanlayici.yeni {
         sure = 500,
         tekrar = false
     }
@@ -25,8 +25,6 @@ function Kullanici_Arayuz:yeni(o)
 end
 
 function Kullanici_Arayuz:ciz()
-    love.graphics.print("Can: ", self.yer.x, self.yer.y)
-
     if self.elleri_goster then
         love.graphics.rectangle("fill", self.el_gosterge_yer.x, self.el_gosterge_yer.y, 30, 30)
     end
@@ -35,7 +33,8 @@ end
 function Kullanici_Arayuz:guncelle(dt)
     local fare_yer = vektor2(love.mouse.getX(), love.mouse.getY())
     self.el_gosterge_yer.x, self.el_gosterge_yer.y = self.oyuncu:yerelx(-16), self.oyuncu:yerely(30)
-    if(carpisma.nokta_dortgene_dahil_mi(fare_yer, self.oyuncu.yer, self.oyuncu.boyut)) then
+
+    if (carpisma.nokta_dortgene_dahil_mi(fare_yer, self.oyuncu.yer, self.oyuncu.boyut)) then
         self.elleri_goster = true
         self.gui_zamanlayici:yeniden_kur()
     else
